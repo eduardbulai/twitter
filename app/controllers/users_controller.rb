4dc702c8fe52fb4682d6212513a75bad07196415
Machiko Yasuda <machikoyasuda@gmail.com>
2013-07-23 11:59:04 -0700

    Change flag Sample App to Welcome.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		 flash[:success] = "Welcome to the Sample App!"
+  		 flash[:success] = "Welcome."
       	redirect_to @user
 
   	else
@@ -18,11 +18,11 @@ class UsersController < ApplicationController
   	end
   end
 
-  
+
 
 
   private
- 
+
   def user_params
   	params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
