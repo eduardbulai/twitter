28b362f891ff828efbf3ee1db3d00cda90950f87
Enddy Dumbrique <enddynayn@hotmail.com>
2013-07-22 18:58:13 -0700

    add Users controller with a new action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Twitter::Application.routes.draw do
+  get "users/new"
   root to: 'static_pages#home'
+  #match '/signup', to: 'users#new'
 
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
