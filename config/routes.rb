cd59858f212840f0cd146131cbd3dd8a8400702a
Enddy Dumbrique <enddynayn@hotmail.com>
2013-07-23 22:53:28 -0700

    Generate migration add_remember_token_to_users to remember user even after browser is closed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Twitter::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
   root to: 'static_pages#home'
 
-  get '/signup', to: 'users#new', via:'get'
+  get '/signup',  to: 'users#new', 			via: 'get'
+  get '/signin',  to: 'sessions#new', 		via: 'get'
+  get '/signout', to: 'sessions#destroy', 	via: 'delete'
 
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
+  get '/help', 		to: 'static_pages#help'
+  get '/about', 	to: 'static_pages#about'
+  get '/contact', 	to: 'static_pages#contact'
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe User do
-<<<<<<< HEAD
+
   
   before { @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
 
@@ -11,6 +11,7 @@ describe User do
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   
@@ -99,19 +100,4 @@ describe User do
 end
 
 
-=======
-  before do
-    @user = User.new(name: "Example User", email: "user@example.com", salt: "123123123123", fish:"123123123123")
-  end
 
-  subject { @user } # set it to @user
-
-  it { should respond_to (:name) }
-  it { should respond_to (:email) }
-  it { should be_valid }
-
-  # expect(subject.salt).not_to be_nil
-  # expect(subject.fish).not_to be_nil
-
-end
->>>>>>> 467c756afe44ee67953b48f038c69514065c498c
