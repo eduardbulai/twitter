8e971fe8d314139cabbc371d437ce6cb892f7fc8
Enddy Dumbrique <enddynayn@hotmail.com>
2013-07-29 11:57:08 -0700

    commit to switch branch

--- db/schema.rb
+++ db/schema.rb
@@ -1,35 +1,27 @@
-ActiveRecord::Schema.define(version: 20130727000913) do
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20130726182727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "afinn_scores", force: true do |t|
-    t.string   "term"
-    t.integer  "score"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "afinn_scores", ["term"], name: "index_afinn_scores_on_term", using: :btree
-
   create_table "queries", force: true do |t|
-    t.integer  "user_id"
-    t.string   "query_term"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "queries", ["user_id"], name: "index_queries_on_user_id", using: :btree
-
-  create_table "query_results", force: true do |t|
-    t.integer  "query_id"
     t.string   "word"
     t.integer  "frequency"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "query_results", ["query_id"], name: "index_query_results_on_query_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -42,5 +34,12 @@ ActiveRecord::Schema.define(version: 20130727000913) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
+
+  create_table "words", force: true do |t|
+    t.string   "word"
+    t.integer  "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
