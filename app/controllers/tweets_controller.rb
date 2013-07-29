834a372dea8028c7068c6ae02d9fb29b83e70350
Machiko Yasuda <machikoyasuda@gmail.com>
2013-07-29 10:54:26 -0700

    Clean up Tweets Controller.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,46 +1,31 @@
 class TweetsController < ApplicationController
 
- 
   MAX_TWEETS = 20
   DEFAULT_SEARCH = "@GA"
   LANG = 'en'
+
   def index
     @tweets = Twitter.search(params[:q] ||= DEFAULT_SEARCH, :count => MAX_TWEETS, :lang =>LANG).results
 
-#Array of search query tweets
+    # Array of search query tweets
     @array_tweets = []
     @tweets.each do |tweet|
     	@array_tweets << tweet.text
     end
 
-#Filters the tweet query
-@words = @array_tweets.join(" ")
-@clean_words = @words.downcase.split(/\W+/)
+    # Filters the tweet query, removes symbols
+    @words = @array_tweets.join(" ")
+    @clean_words = @words.downcase.split(/\W+/)
 
-#puts frequency of word into hash
-@freqs = Hash.new(0)
-@clean_words.each { |w| @freqs[w] += 1 }
+    # Count frequency of words and put into @freqs hash
+    @freqs = Hash.new(0)
+    @clean_words.each { |w| @freqs[w] += 1 }
 
-####puts the frequency into a hash
-@freqs.each do |k, v|
-  Tweet.create word: k.downcase, frequency: v
-end
+    # Save frequency of words into database
+    @freqs.each do |k, v|
+      Tweet.create word: k.downcase, frequency: v
+    end
 
-@database = Tweet.all
-
-  end
-
-
-
-
-
-def show
-    user = if params[:id].starts_with? "@"
-             params[:id][1..-1]
-           else
-             params[:id].to_i
-           end
-
-    @tweets = Twitter.user_timeline(user)
+    @database = Tweet.all
   end
 end
