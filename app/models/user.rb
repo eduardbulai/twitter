9c1515336bfd46c210d52fbb7fc7548a5d3ede42
Enddy Dumbrique <enddynayn@hotmail.com>
2013-07-28 19:47:05 -0700

    session implementation complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
   before_save { self.email = email.downcase }
   before_create :create_remember_token
+  #
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
@@ -10,6 +11,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }
 
+  ####################Sessions tokens
   def User.new_remember_token
   	SecureRandom.urlsafe_base64
   end
@@ -17,7 +19,7 @@ class User < ActiveRecord::Base
   def User.encrypt(token)
   	Digest::SHA1.hexdigest(token.to_s)
   end
-
+###############
   private
   	def create_remember_token
   		self.remember_token = User.encrypt(User.new_remember_token)
