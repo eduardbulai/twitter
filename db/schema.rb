b6760599789515e8a0e8c78bfad5ce5cbf05f6ef
Machiko Yasuda <machikoyasuda@gmail.com>
2013-07-29 10:09:48 -0700

    Changes to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130723003832) do
+ActiveRecord::Schema.define(version: 20130726182727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "queries", force: true do |t|
+    t.string   "word"
+    t.integer  "frequency"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -22,6 +29,16 @@ ActiveRecord::Schema.define(version: 20130723003832) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
+  end
+
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
+
+  create_table "words", force: true do |t|
+    t.string   "word"
+    t.integer  "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
